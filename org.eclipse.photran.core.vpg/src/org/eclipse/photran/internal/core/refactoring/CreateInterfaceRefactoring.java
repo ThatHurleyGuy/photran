8437f26fa5715b8600e1297f9c15f56ac36e9356
Sean Hurley <sean.hurley6@gmail.com>
2012-11-28 21:06:22 -0600

    Adding stub refactoring

---
+++ org.eclipse.photran.core.vpg/src/org/eclipse/photran/internal/core/refactoring/CreateInterfaceRefactoring.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2012 TODO COMPANY NAME and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    seanhurley (TODO COMPANY NAME) - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.photran.internal.core.refactoring;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.photran.internal.core.refactoring.infrastructure.FortranEditorRefactoring;
+
+/**
+ * 
+ * @author seanhurley
+ */
+public class CreateInterfaceRefactoring extends FortranEditorRefactoring
+{
+
+    /* (non-Javadoc)
+     * @see org.eclipse.rephraserengine.core.vpg.refactoring.VPGRefactoring#doCheckInitialConditions(org.eclipse.ltk.core.refactoring.RefactoringStatus, org.eclipse.core.runtime.IProgressMonitor)
+     */
+    @Override
+    protected void doCheckInitialConditions(RefactoringStatus status, IProgressMonitor pm)
+        throws org.eclipse.rephraserengine.core.vpg.refactoring.VPGRefactoring.PreconditionFailure
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.rephraserengine.core.vpg.refactoring.VPGRefactoring#doCheckFinalConditions(org.eclipse.ltk.core.refactoring.RefactoringStatus, org.eclipse.core.runtime.IProgressMonitor)
+     */
+    @Override
+    protected void doCheckFinalConditions(RefactoringStatus status, IProgressMonitor pm)
+        throws org.eclipse.rephraserengine.core.vpg.refactoring.VPGRefactoring.PreconditionFailure
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.rephraserengine.core.vpg.refactoring.VPGRefactoring#doCreateChange(org.eclipse.core.runtime.IProgressMonitor)
+     */
+    @Override
+    protected void doCreateChange(IProgressMonitor pm) throws CoreException,
+        OperationCanceledException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ltk.core.refactoring.Refactoring#getName()
+     */
+    @Override
+    public String getName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
